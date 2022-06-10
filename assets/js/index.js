15bdd5f411319188c9f1c8e75ed2ad1f15de152c
Rami Elsayed <ramielsayed123@outlook.com>
2022-06-10 22:14:56 +0100

    render cities from ls and fixed rendering func

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 const searchHistory = document.getElementById("search-history");
+const searchForm = document.getElementById("search-form");
+const searchButton = document.getElementById("search-button");
+const citiesList = document.createElement("ul");
 
 const readFromLs = (key, defaultValue) => {
   const lsData = localStorage.getItem(key);
@@ -10,18 +13,24 @@ const readFromLs = (key, defaultValue) => {
     return defaultValue;
   }
 };
-
+const writeToLs = (key,value) => {
+        const strinfiedValue = JSON.stringify(value);
+        localStorage.setItem(key,strinfiedValue);
+}
 const renderSearches = () => {
+    citiesList.innerHTML = '';
   const recentSearches = readFromLs("recentSearches", []);
   if (recentSearches.length) {
+    const alert = document.getElementById("alert");
+    if (alert) {
+        alert.remove();
+    }
     const getCity = (city) => { 
         const searchedCity = document.createElement("li");
         searchedCity.classList.add("history-cities");
         searchedCity.textContent = city;
         return searchedCity
     };
-    
-    const citiesList = document.createElement("ul");
     recentSearches.map(getCity).forEach(x => citiesList.appendChild(x));
     console.log(citiesList)
     // `<ul class="search-table-elements search-history"> </ul>`;
@@ -31,6 +40,7 @@ const renderSearches = () => {
     const alert = document.createElement("div");
     alert.textContent = "you have no recent searches";
     alert.classList.add("search-alert");
+    alert.setAttribute("id","alert");
     searchHistory.append(alert);
   }
 };
@@ -42,9 +52,24 @@ const onRecentSearch = (event) => {
     console.log(city);
   }
 };
+
+const onFormSubmit = (event) => {
+        event.preventDefault();
+        const cityName = document.getElementById("search-input").value;
+        if (cityName) {
+             const recentSearches = readFromLs("recentSearches", []);
+             if (!recentSearches.includes(cityName)) {
+                  recentSearches.push(cityName);
+             }
+             writeToLs("recentSearches", recentSearches);
+             
+             renderSearches();
+        }
+}
 const onReady = () => {
   renderSearches();
 };
 
 searchHistory.addEventListener("click", onRecentSearch);
+searchButton.addEventListener("click", onFormSubmit);
 window.onload = onReady;
